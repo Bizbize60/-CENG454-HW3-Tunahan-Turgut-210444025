6f9defe7a75731983af85d82d85aacd628340eb4
Bizbize60 <s210444025@stu.thk.edu.tr>
2026-05-18 00:06:24 +0300

    feat: composite pattern added

--- My project/Assets/Scripts/Enemies/Pooling/EnemyPool.cs
+++ My project/Assets/Scripts/Enemies/Pooling/EnemyPool.cs
@@ -3,47 +3,39 @@ using UnityEngine;
 
 public class EnemyPool : MonoBehaviour
 {
-    [SerializeField] private IEnemy entityPrefab;
+    [SerializeField] private Enemy entityPrefab;
     [SerializeField] private int startupPoolCapacity = 15;
 
-    private readonly Queue<IEnemy> entityQueue = new Queue<IEnemy>();
+    private readonly Queue<Enemy> entityQueue = new Queue<Enemy>();
 
     private void Awake()
     {
         for (int index = 0; index < startupPoolCapacity; index++)
-        {
             InstantiateNewEntity();
-        }
     }
 
-    private Enemy InstantiateNewEntity()
+    private void InstantiateNewEntity()
     {
-        IEnemy newEntity = Instantiate(entityPrefab, transform);
+        Enemy newEntity = Instantiate(entityPrefab, transform);
         newEntity.gameObject.SetActive(false);
         entityQueue.Enqueue(newEntity);
-        return newEntity;
     }
 
-    
-    public IEnemy GetEnemy(Vector3 spawnPosition, Quaternion spawnRotation)
+    public Enemy GetEnemy(Vector3 spawnPosition, Quaternion spawnRotation)
     {
         if (entityQueue.Count == 0)
-        {
             InstantiateNewEntity();
-        }
 
-        IEnemy activeEntity = entityQueue.Dequeue();
-
+        Enemy activeEntity = entityQueue.Dequeue();
         activeEntity.transform.position = spawnPosition;
         activeEntity.transform.rotation = spawnRotation;
-        
         activeEntity.ResetEntity();
         activeEntity.gameObject.SetActive(true);
 
         return activeEntity;
     }
 
-    public void ReturnEnemy(IEnemy returningEntity)
+    public void ReturnEnemy(Enemy returningEntity)
     {
         returningEntity.gameObject.SetActive(false);
         entityQueue.Enqueue(returningEntity);
