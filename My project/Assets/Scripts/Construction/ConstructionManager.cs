cd68f180df3ccff4a927da1d9b818d08d85b2bbc
Bizbize60 <s210444025@stu.thk.edu.tr>
2026-05-17 17:00:27 +0300

    feat: update selection logic to use Ground-based naming conventions

--- My project/Assets/Scripts/Construction/ConstructionManager.cs
+++ My project/Assets/Scripts/Construction/ConstructionManager.cs
@@ -4,16 +4,47 @@ public class ConstructionManager : MonoBehaviour {
 
     public static ConstructionManager instance;
 
-    void Awake() 
-    {
-        if (instance != null)
-        {
-            Debug.LogError("More than one ConstructionManager in scene!");
-            return;
-        }
-        instance = this;
-    }
+    void Awake() { /* Singleton Kontrolü */ }
 
     public GameObject constructionEffect;
     public GameObject liquidationEffect;
+
+    private TurretBlueprint turretToConstruct;
+    private Ground selectedGround;
+
+    public GroundUI groundUI;
+
+    public bool CanConstruct { get { return turretToConstruct != null; } }
+    public bool HasRequiredFunds { get { return PlayerStats.Money >= turretToConstruct.cost; } }
+
+    public void SelectGround(Ground ground)
+    {
+        if (selectedGround == ground)
+        {
+            DeselectGround();
+            return;
+        }
+
+        selectedGround = ground;
+        turretToConstruct = null;
+
+        groundUI.SetTarget(ground);
+    }
+
+    public void DeselectGround()
+    {
+        selectedGround = null;
+        groundUI.Hide();
+    }
+
+    public void SelectTurretToConstruct(TurretBlueprint turret)
+    {
+        turretToConstruct = turret;
+        DeselectGround();
+    }
+
+    public TurretBlueprint GetTurretToConstruct()
+    {
+        return turretToConstruct;
+    }
 }
