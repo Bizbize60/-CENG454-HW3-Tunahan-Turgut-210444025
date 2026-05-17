212eb51474e12089912bab356a2cadeda151affd
Bizbize60 <s210444025@stu.thk.edu.tr>
2026-05-17 12:17:13 +0300

    feat: implement basic timer mechanism for wave controller

---
+++ My project/Assets/Scripts/WaveSpawner/WaveController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class WaveController : MonoBehaviour
+{
+    [SerializeField] private float waveInterval = 7.5f;
+    private float currentTimer = 4f;
+
+    private void Update()
+    {
+        if (currentTimer <= 0f)
+        {
+            currentTimer = waveInterval;
+        }
+        
+        currentTimer -= Time.deltaTime;
+    }
+}
