fa80c27e02561af99ca55a0f6f44d6f4c9f82859
Bizbize60 <s210444025@stu.thk.edu.tr>
2026-05-17 12:20:23 +0300

    feat: integrate wave generation coroutine with spawn intervals

--- My project/Assets/Scripts/WaveSpawner/WaveController.cs
+++ My project/Assets/Scripts/WaveSpawner/WaveController.cs
@@ -3,18 +3,38 @@ using System.Collections;
 
 public class WaveController : MonoBehaviour
 {
+    [SerializeField] private EnemyPool monsterPool;
+    [SerializeField] private Transform spawnOrigin;
+    
     [SerializeField] private float waveInterval = 7.5f;
+    [SerializeField] private float spawnInterval = 0.3f; 
+    
     private float currentTimer = 4f;
+    private int currentWave = 0; 
+
+    private IEnumerator GenerateWave()
+    {
+        currentWave++;
+        
+        for (int i = 0; i < currentWave; i++)
+        {
+            InstantiateEntity();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
 
     private void Update()
     {
         if (currentTimer <= 0f)
         {
+            StartCoroutine(GenerateWave());
             currentTimer = waveInterval;
         }
         
         currentTimer -= Time.deltaTime;
     }
+
     private void InstantiateEntity()
     {
         Enemy monster = monsterPool.GetEnemy(spawnOrigin.position, spawnOrigin.rotation);
