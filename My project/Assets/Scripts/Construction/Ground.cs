c58bcd0ba12d38521199567c5ea4092a4bba8dfc
Bizbize60 <s210444025@stu.thk.edu.tr>
2026-05-17 17:48:34 +0300

    fix: simplify Ground logic and remove redundant blueprint dependencies

--- My project/Assets/Scripts/Construction/Ground.cs
+++ My project/Assets/Scripts/Construction/Ground.cs
@@ -1,87 +1,46 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Ground : MonoBehaviour {
-
+public class Ground : MonoBehaviour 
+{
     public Color hoverHighlightColor;
-    public Color insufficientFundsColor;
     public Vector3 constructionOffset;
 
-    
-    public GameObject currentTurret;
-    public TurretBlueprint currentBlueprint;
-  
-
+    private GameObject deployedTurret;
     private Renderer meshRenderer;
     private Color originalColor;
 
-    private ConstructionManager constructionManager;
-
-    void Start ()
+    private void Start()
     {
         meshRenderer = GetComponent<Renderer>();
         originalColor = meshRenderer.material.color;
-
-        constructionManager = ConstructionManager.instance;
     }
 
-    public Vector3 GetConstructionPosition ()
-    {
-        return transform.position + constructionOffset;
-    }
-
-    void OnMouseEnter ()
+    private void OnMouseDown()
     {
         if (EventSystem.current.IsPointerOverGameObject()) return;
-        if (!constructionManager.CanConstruct) return;
 
-        if (constructionManager.HasRequiredFunds)
+        if (deployedTurret != null)
         {
-            meshRenderer.material.color = hoverHighlightColor;
-        } 
-        else
-        {
-            meshRenderer.material.color = insufficientFundsColor;
+            Debug.Log("Area occupied! - TODO: Display on screen.");
+            return;
         }
+
+        deployedTurret = ConstructionManager.instance.ConstructTurret(
+            transform.position + constructionOffset,
+            transform.rotation
+        );
     }
 
-    void OnMouseExit ()
+    private void OnMouseEnter()
+    {
+        if (EventSystem.current.IsPointerOverGameObject()) return;
+        
+        meshRenderer.material.color = hoverHighlightColor;
+    }
+
+    private void OnMouseExit()
     {
         meshRenderer.material.color = originalColor;
-    }
-
-    void OnMouseDown ()
-    {
-        if (EventSystem.current.IsPointerOverGameObject()) return;
-
-        if (currentTurret != null)
-        {
-            constructionManager.SelectGround(this);
-            return;
-        }
-
-        if (!constructionManager.CanConstruct) return;
-
-        ConstructTurret(constructionManager.GetTurretToConstruct());
-    }
-
-    void ConstructTurret (TurretBlueprint blueprint)
-    {
-        if (PlayerStats.Money < blueprint.cost)
-        {
-            Debug.Log("Not enough money to construct that!");
-            return;
-        }
-
-        PlayerStats.Money -= blueprint.cost;
-
-        GameObject _turret = Instantiate(blueprint.prefab, GetConstructionPosition(), Quaternion.identity);
-        currentTurret = _turret;
-        currentBlueprint = blueprint;
-
-        GameObject effect = Instantiate(constructionManager.constructionEffect, GetConstructionPosition(), Quaternion.identity);
-        Destroy(effect, 5f);
-
-        Debug.Log("Turret constructed!");
     }
 }
