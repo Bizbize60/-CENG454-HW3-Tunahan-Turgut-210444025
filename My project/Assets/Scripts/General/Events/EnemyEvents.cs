a3235722d06a4bf82f7e2a808663af5bb9d285b9
Bizbize60 <s210444025@stu.thk.edu.tr>
2026-05-17 12:50:29 +0300

    feat: add invocation method for destination event

--- My project/Assets/Scripts/General/Events/EnemyEvents.cs
+++ My project/Assets/Scripts/General/Events/EnemyEvents.cs
@@ -3,4 +3,9 @@ using System;
 public static class EnemyEvents
 {
     public static event Action<Enemy> OnEnemyReachedEnd;
+
+    public static void RaiseEnemyReachedEnd(Enemy targetEntity)
+    {
+        OnEnemyReachedEnd?.Invoke(targetEntity);
+    }
 }
