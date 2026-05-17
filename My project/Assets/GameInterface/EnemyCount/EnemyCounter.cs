05d7c19de6ea7f7e19bc5e268b814a79ea12787d
Bizbize60 <s210444025@stu.thk.edu.tr>
2026-05-17 14:24:19 +0300

    feat: bind wave countdown events to UI text updates

--- My project/Assets/GameInterface/EnemyCount/EnemyCounter.cs
+++ My project/Assets/GameInterface/EnemyCount/EnemyCounter.cs
@@ -4,4 +4,19 @@ using UnityEngine;
 public class EnemyCounter : MonoBehaviour
 {
     [SerializeField] private TMP_Text timerDisplayText;
+
+    private void OnEnable()
+    {
+        WaveEvents.OnCountdownChanged += RefreshTimerUI;
+    }
+
+    private void OnDisable()
+    {
+        WaveEvents.OnCountdownChanged -= RefreshTimerUI;
+    }
+
+    private void RefreshTimerUI(float timeLeft)
+    {
+        timerDisplayText.text = Mathf.CeilToInt(timeLeft).ToString();
+    }
 }
