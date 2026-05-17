3a9fa4833a0faab02310d451d66c206405b8139c
Bizbize60 <s210444025@stu.thk.edu.tr>
2026-05-17 13:07:10 +0300

    feat: add invocation wrappers for wave events

--- My project/Assets/Scripts/General/Events/WaveEvents.cs
+++ My project/Assets/Scripts/General/Events/WaveEvents.cs
@@ -5,4 +5,19 @@ public static class WaveEvents
     public static event Action<float> OnCountdownChanged;
     public static event Action<int> OnWaveStarted;
     public static event Action<Enemy> OnEnemySpawned;
+
+    public static void RaiseCountdownChanged(float timerValue)
+    {
+        OnCountdownChanged?.Invoke(timerValue);
+    }
+
+    public static void RaiseWaveStarted(int currentWave)
+    {
+        OnWaveStarted?.Invoke(currentWave);
+    }
+
+    public static void RaiseEnemySpawned(Enemy targetEntity)
+    {
+        OnEnemySpawned?.Invoke(targetEntity);
+    }
 }
